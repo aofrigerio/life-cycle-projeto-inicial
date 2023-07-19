d514c536727b300c83e6d6a00a24386f99fe5c32
Alan <aofrigerio@gmail.com>
2023-07-18 21:54:26 -0300

    first commit

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit, OnChanges {
+
+  @Input() itemQuevaiSerEditado! : Item;
+  editando = false
+  textoBtn = 'Salvar Item'
+
+  valorItem! : string
+
+  constructor(private service: ListaDeCompraService) { }
+
+  ngOnInit(): void { }
+
+  adicionarItem(){
+      this.service.postListaDeCompra(this.valorItem)
+  }
+
+  editarItem(){
+    this.service.putListaDeCompra(this.itemQuevaiSerEditado, this.valorItem)
+    this.valorItem = ''
+    this.editando = false
+    this.textoBtn = 'Salvar item'
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes['itemQuevaiSerEditado'].currentValue)
+    if(!changes['itemQuevaiSerEditado'].firstChange){
+      this.editando = true
+      this.textoBtn = 'Editar Item'
+      this.valorItem = this.itemQuevaiSerEditado?.nome
+    }
+  }
+}
